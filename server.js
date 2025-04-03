af58f1d71e66ba629caa535d5d7795ca8b5c3eab
Bikash141 <bikashprasad1412@gmail.com>
2025-04-03 13:13:17 +0530

    change in server.js

--- server.js
+++ server.js
@@ -42,4 +42,4 @@ app.use((err,req,res,next) => {
     });
 });
 
-app.listen(3000, ()=> console.log('Server is running on port 3000'));
+app.listen(3000,'0.0.0.0', ()=> console.log('Server is running on port 3000'));
